5758bc1873e7c56f901d194cc22e3b3bb7d50faa
Daniel Nguyen <daniel.nguyen@mrjaffesclass.com>
2024-02-21 12:46:01 -0800

    Changed Constants declaration

---
+++ connect-four-CSA/tictactoe/State.java
@@ -0,0 +1,82 @@
+package tictactoe;
+
+/**
+ * Tic-Tac-Toe state variables.
+ */
+public class State
+{
+    private int gameState = Constants.STANDBY;
+    private int whoseMove = Constants.RED;
+    private String redName = "";
+    private String blackName = "";
+    private int[][] board = new int[Constants.BOARD_ROW][Constants.BOARD_COL];
+
+    public boolean isWinner() {
+        int total;
+        for (int row=0; row<Constants.BOARD_SIZE; row++) {
+            total = getBoardCell(row, 0) + getBoardCell(row,1) + getBoardCell(row,2);
+            if (total == -3 || total == 3) return true;
+        }
+        for (int col=0; col<Constants.BOARD_SIZE; col++) {
+            total = getBoardCell(0, col) + getBoardCell(1,col) + getBoardCell(2, col);
+            if (total == -3 || total == 3) return true;
+        }
+        total = getBoardCell(0, 0) + getBoardCell(1,1) + getBoardCell(2, 2);
+        if (total == -3 || total == 3) return true;
+        total = getBoardCell(2, 0) + getBoardCell(1,1) + getBoardCell(0, 2);
+        if (total == -3 || total == 3) return true;
+        return false;
+    }
+
+    public boolean isTie() {
+        for (int row=0; row<Constants.BOARD_SIZE; row++) {
+            for (int col=0; col<Constants.BOARD_SIZE; col++) {
+                if (getBoardCell(row,col) == Constants.BLANK) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
+
+    public int getWhoseMove() {
+        return whoseMove;
+    }
+
+    public void setWhoseMore(int whoseMove) {
+        this.whoseMove = whoseMove;
+    }
+
+    public String getRedName() {
+        return redName;
+    }
+
+    public void setredName(String redName) {
+        this.redName = redName;
+    }
+
+    public String getblackName() {
+        return blackName;
+    }
+
+    public void setBlackName(String blackName) {
+        this.blackName = blackName;
+    }
+
+    public int getBoardCell(int row, int col) {
+        return this.board[row][col];
+    }
+
+    public void setBoardCell(int row, int col, int value) {
+        this.board[row][col] = value;
+    }
+
+}
